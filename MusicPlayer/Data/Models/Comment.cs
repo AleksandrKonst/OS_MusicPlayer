b691aac2ae390259d46313fcea89b51e6b8c0117
AleksandrKonst <genkonst5@gmail.com>
2022-11-20 20:18:07 +0300

    add http get/post

---
+++ MusicPlayer/Data/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace MPlayWebAPI.Models.Data
+{
+    public class Comment
+    {
+        public int commentId { get; set; }
+
+        public string text { get; set; }
+
+        public int musicId { get; set; }
+    }
+}
--- MusicPlayer/ViewModel/MainWindow.xaml.cs
+++ MusicPlayer/ViewModel/MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using System.Text.Json;
+using System.Collections.Generic;
+using MPlayWebAPI.Models.Data;
+using System.Windows.Controls;
 
 namespace MusicPlayer
 {
@@ -14,6 +20,8 @@ namespace MusicPlayer
         private MediaPlayer player = new MediaPlayer();
         private bool PlayOrNot = true;
         private bool dragStarted = false;
+        private string name;
+        private int id;
 
         public MainWindow()
         {
@@ -43,13 +51,47 @@ namespace MusicPlayer
 
                     System.Windows.Controls.Image img = new System.Windows.Controls.Image();
                     img.Source = bitmap;
-                    ImageTrack.Source = img.Source;
+                    ImageTrack.Source = img.Source;   
                 }
                 
                 DispatcherTimer timer = new DispatcherTimer();
                 timer.Interval = TimeSpan.FromSeconds(1);
                 timer.Tick += timer_Tick;
                 timer.Start();
+
+                name = audioFile.Tag.Title;
+                GetText(name);
+                GetComment(name);
+            }
+        }
+
+        public async Task GetText(string name)
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync($"https://localhost:7119/api/Music/{name}");
+                TextSong.Text = await result.Content.ReadAsStringAsync();
+            }
+        }
+
+        public async Task GetComment(string name, int page = 1)
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync($"https://localhost:7119/api/Comment/{name}/{page}");
+                var stream = await result.Content.ReadAsStreamAsync();
+                List<Comment> comment = await JsonSerializer.DeserializeAsync<List<Comment>>(stream);
+                foreach (Comment el in comment)
+                {
+                    Grid grid = new Grid();
+                    grid.Style = (Style)Resources["Grid1"];
+                    TextBlock textBlock = new TextBlock();
+                    textBlock.Style = (Style)Resources["Text1"];
+                    textBlock.Text = el.text;
+                    grid.Children.Add(textBlock);
+                    CommentGrid.Children.Add(grid);
+                }
+                id = comment.First().musicId;
             }
         }
 
@@ -99,6 +141,24 @@ namespace MusicPlayer
                 player.Pause();
                 this.player.Position = TimeSpan.FromSeconds(Slider_Music.Value / 1000);
             }              
+        }
+
+        private async Task Button_Click_Async()
+        {
+            using (var client = new HttpClient())
+            {
+                HttpContent content = new StringContent(SendText.Text);
+
+                var result = await client.PostAsync($"https://localhost:7119/api/Comment/{id}/{SendText.Text}", content); //body problem
+
+                CommentGrid.Children.Clear();
+                GetComment(name);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click_Async();
         }
     }
 }
